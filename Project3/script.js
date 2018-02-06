a0c054303aa603658069cd741f027ebf0aac8f60
amandaluhan <ahan01@risd.edu>
2018-02-05 20:53:52 -0500

    changes

---
+++ Project3/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+	//targets .dotOne
+	$(".dotOne").mouseenter(function(){
+		$(".one img").css("display", "block");
+	})
+
+	$(".dotOne").mouseleave(function(){
+		$(".one img").css("display", "none");
+	})
+
+	//targets .dotTwo
+	$(".dotTwo").mouseenter(function(){
+		$(".two img").css("display", "block");
+	})
+
+	$(".dotTwo").mouseleave(function(){
+		$(".two img").css("display", "none");
+	})
+
+	//targets .dotThree
+	$(".dotThree").mouseenter(function(){
+		$(".three img").css("display", "block");
+	})
+
+	$(".dotThree").mouseleave(function(){
+		$(".three img").css("display", "none");
+	})
+
+	//targets .dotFour
+	$(".dotFour").mouseenter(function(){
+		$(".four img").css("display", "block");
+	})
+
+	$(".dotFour").mouseleave(function(){
+		$(".four img").css("display", "none");
+	})
+
+	//targets .dotFive
+	$(".dotFive").mouseenter(function(){
+		$(".five img").css("display", "block");
+	})
+
+	$(".dotFive").mouseleave(function(){
+		$(".five img").css("display", "none");
+	})
+
+})
+
---
+++ Project4/portfolio.js
@@ -0,0 +1,21 @@
+// $(document).ready(function(){
+// 	$(".header").mouseenter(function(){
+// 		$(".title").css("color", "white");
+// 		$(".header").css("background-color", "#ffb3cc");
+// 	})
+
+// 	$(".header").mouseleave(function(){
+// 		$(".title").css("color", "#D8D8D8");
+// 		$(".header").css("background-color", "white");
+// 	})
+
+// 	$(".headerTwo").mouseenter(function(){
+// 		$(".titleTwo").css("color", "#D8D8D8");
+// 		$(".headerTwo").css("background-color", "white");
+// 	})
+
+// 	$(".headerTwo").mouseleave(function(){	
+// 		$(".titleTwo").css("color", "white");
+// 		$(".headerTwo").css("background-color", "#ffb3cc");
+// 	})
+// })
